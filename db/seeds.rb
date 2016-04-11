f4dcca107632cf955331416d8cfa4f4406b65f6b
salayhin <sirajus@augmedix.com>
2016-04-12 04:43:21 +0600

    Default route added

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,5 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+AdminUser.create!(email: 'admin@gmail.com', password: 'password', password_confirmation: 'password')
